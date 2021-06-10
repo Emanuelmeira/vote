1ab62445baa4b05249575c467bbadd9bb316a8e7
emmit <emmitmeira@gmail.com>
2021-06-10 18:59:37 -0300

    Tabela para computar voto

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,5 +3,13 @@ CREATE TABLE IF NOT EXISTS PAUTA (
   id INT AUTO_INCREMENT  PRIMARY KEY,
   theme VARCHAR(100) NOT NULL,
   session_started_time TIMESTAMP NULL,
-  ends_in INT NULL
+  ends_in INT NULL,
+  open_session BOOLEAN DEFAULT false
+);
+
+CREATE TABLE IF NOT EXISTS VOTO (
+  id INT AUTO_INCREMENT  PRIMARY KEY,
+  associado_id INT NOT NULL,
+  pauta_id INT NOT NULL,
+  voto VARCHAR(4) NOT NULL
 );
