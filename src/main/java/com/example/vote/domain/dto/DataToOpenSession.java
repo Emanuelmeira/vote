38be03108958c8f6e0e2761e30972ac8e558c8c3
emmit <emmitmeira@gmail.com>
2021-06-10 16:12:10 -0300

    Recursos para abrir Pauta para votação

---
+++ src/main/java/com/example/vote/domain/dto/DataToOpenSession.java
@@ -0,0 +1,14 @@
+package com.example.vote.domain.dto;
+
+public class DataToOpenSession {
+
+    private Long timeInMinutes;
+
+    public Long getTimeInMinutes() {
+        return timeInMinutes;
+    }
+
+    public void setTimeInMinutes(Long timeInMinutes) {
+        this.timeInMinutes = timeInMinutes;
+    }
+}
